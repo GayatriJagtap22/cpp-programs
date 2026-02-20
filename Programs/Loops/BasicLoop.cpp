0a4787392dc184d5cf726b8213208dbbd68fcdc8
GayatriJagtap22 <gayatrisjagtap.2806@gmail.com>
2026-02-20 16:11:28 +0530

    Update BasicLoop.cpp

--- Programs/Loops/BasicLoop.cpp
+++ Programs/Loops/BasicLoop.cpp
@@ -1,1 +1,15 @@
 
+// Print sum of first n natural numbers:
+ #include<iostream>
+ using namespace std;
+
+int main()  {
+    int i,n,sum=0;
+    cout<<"Enter number n: ";
+    cin>>n;
+    for(i=1;i<=n;i++)   {
+     sum = sum + i;
+     cout<<sum;
+return 0;
+    }
+  }
