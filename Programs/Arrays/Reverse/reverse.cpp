73348c11098a6f7ad8f8980e2125792a2efb7ca0
GayatriJagtap22 <gayatrisjagtap.2806@gmail.com>
2026-02-20 22:58:34 +0530

    Create reverse.cpp

---
+++ Programs/Arrays/Reverse/reverse.cpp
@@ -0,0 +1,24 @@
+#include<iostream>
+using namespace std;
+
+void printarr(int *arr, int n) {
+    for (int i = 0; i < n; i++) {
+        cout << arr[i] << ",";
+    }
+    cout << endl;
+}
+
+int main() {
+    int arr[] = {5, 7, 9, 2, 1};
+    int n = sizeof(arr) / sizeof(int);
+    int start = 0, end = n - 1;
+
+    while (start < end) {
+        swap(arr[start], arr[end]);
+        start++;
+        end--;
+    }
+
+    printarr(arr, n);
+    return 0;
+}
