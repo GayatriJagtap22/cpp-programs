b9014c25784b94f9aacc9ea6659c917bd06bf453
GayatriJagtap22 <gayatrisjagtap.2806@gmail.com>
2026-02-20 15:59:54 +0530

    Create Largestarray.cpp

---
+++ Programs/Arrays/Largestarray.cpp
@@ -0,0 +1,14 @@
+//                    LARGEST IN THE ARRAY
+
+// int main() {
+// int arr[] = {7, 5, 2, 4, 9};
+// int n = sizeof(arr) / sizeof(int);
+
+// int max = arr[0];
+// for(int i=0;i<n;i++)  {
+//     if(arr[i] > max)
+//     max = arr[i];
+// }
+// cout<< max;
+// return 0;
+// }
