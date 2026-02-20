939150f25eb84d78c8623c60d9f75bb11d068d33
GayatriJagtap22 <gayatrisjagtap.2806@gmail.com>
2026-02-20 16:05:54 +0530

    Update Largestarray.cpp

--- Programs/Arrays/Largestarray.cpp
+++ Programs/Arrays/Largestarray.cpp
@@ -1,14 +1,17 @@
 //                    LARGEST IN THE ARRAY
 
-// int main() {
-// int arr[] = {7, 5, 2, 4, 9};
-// int n = sizeof(arr) / sizeof(int);
+#include<iostream>
+using namespace std;
 
-// int max = arr[0];
-// for(int i=0;i<n;i++)  {
-//     if(arr[i] > max)
-//     max = arr[i];
-// }
-// cout<< max;
-// return 0;
-// }
+int main() {
+int arr[] = {7, 5, 2, 4, 9};
+int n = sizeof(arr) / sizeof(int);
+
+int max = arr[0];
+for(int i=0;i<n;i++)  {
+    if(arr[i] > max)
+    max = arr[i];
+}
+cout<< max;
+return 0;
+}
