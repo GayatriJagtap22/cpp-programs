95b95dcf400526bff95bb063f9bc3bda420d42e5
GayatriJagtap22 <gayatrisjagtap.2806@gmail.com>
2026-02-20 16:05:07 +0530

    Create Linearsearch.cpp

---
+++ Programs/Arrays/Linearsearch.cpp
@@ -0,0 +1,24 @@
+//             LINEAR SEARCH (find key=10)
+
+int linearsearch(int *arr, int n, int key) {
+    for (int i = 0; i < n; i++) {
+        if (arr[i] == key) {
+            return i;  
+        }
+    }
+    return -1;            // return -1 if key not found
+}
+
+int main() {
+    int arr[] = {1, 3, 5, 10}; 
+    int n = sizeof(arr) / sizeof(arr[0]);
+
+    int index = linearsearch(arr, n, 10);
+    if (index != -1) {
+        cout << "Element found at index: " << index << endl;
+    } else {
+        cout << "Element not found" << endl;
+    }
+
+    return 0;
+}
