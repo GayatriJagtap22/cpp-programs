d9e7d438c7c90b7f4691d4d564f45bf594756d9c
GayatriJagtap22 <gayatrisjagtap.2806@gmail.com>
2026-02-20 16:07:09 +0530

    Update Arrayfunc.cpp

--- Programs/Arrays/Arrayfunc.cpp
+++ Programs/Arrays/Arrayfunc.cpp
@@ -1,15 +1,18 @@
 //                             ARRAY IN FUNCTION           
-// 
-// void printarr(int nums[], int n)    {
-// for(int i=0; i<n; i++)    {
-//     cout<<nums[i]<<",";
-// }
-// cout<<endl;
-// }
-// int main()   {
-//     int arr[] = {1,2,3,4,5};
-//     int size = sizeof(arr) / sizeof(int);
-// cout<< size <<endl;
-// printarr(arr,size);
-// return 0;
-// }
+
+#include<iostream>
+using namespace std;
+
+void printarr(int nums[], int n)    {
+for(int i=0; i<n; i++)    {
+    cout<<nums[i]<<",";
+}
+cout<<endl;
+}
+int main()   {
+    int arr[] = {1,2,3,4,5};
+    int size = sizeof(arr) / sizeof(int);
+cout<< size <<endl;
+printarr(arr,size);
+return 0;
+}
