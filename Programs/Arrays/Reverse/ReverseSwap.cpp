1fb1b96b94e2c1619ccf30d7a53c78d158ed432f
GayatriJagtap22 <gayatrisjagtap.2806@gmail.com>
2026-02-20 22:46:14 +0530

    Update ReverseSwap.cpp

--- Programs/Arrays/Reverse/ReverseSwap.cpp
+++ Programs/Arrays/Reverse/ReverseSwap.cpp
@@ -1,1 +1,28 @@
 
+//                               REVERSE BY  SWAPPING
+#include<iostream>
+using namespace std;
+
+ void printarr(int *arr, int n) {
+     for (int i = 0; i < n; i++) {
+         cout << arr[i] << ",";
+     }
+     cout << endl;
+ }
+
+ int main() {
+     int arr[] = {5, 7, 9, 2, 1};
+     int n = sizeof(arr) / sizeof(int);
+     int start = 0, end = n - 1;
+
+     while (start < end) {
+         int temp = arr[start];
+         arr[start] = arr[end];
+         arr[end] = temp; // swap
+         start++;
+         end--;
+     }
+
+     printarr(arr, n);
+     return 0;
+ }
