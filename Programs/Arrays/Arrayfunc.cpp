9c4bdae56f418237a41a7e23daa0e382ca4db370
GayatriJagtap22 <gayatrisjagtap.2806@gmail.com>
2026-02-20 16:03:08 +0530

    Create Arrayfunc.cpp

---
+++ Programs/Arrays/Arrayfunc.cpp
@@ -0,0 +1,15 @@
+//                             ARRAY IN FUNCTION           
+// 
+// void printarr(int nums[], int n)    {
+// for(int i=0; i<n; i++)    {
+//     cout<<nums[i]<<",";
+// }
+// cout<<endl;
+// }
+// int main()   {
+//     int arr[] = {1,2,3,4,5};
+//     int size = sizeof(arr) / sizeof(int);
+// cout<< size <<endl;
+// printarr(arr,size);
+// return 0;
+// }
