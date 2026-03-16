2c60a77eb0745829befbf4943620e1c7e4858c86
Gayatri Jagtap <gayatrisjagtap.2806@gmail.com>
2026-03-16 17:53:27 +0530

    Update and rename Matrix to Matrix.cpp

---
+++ Programs/Arrays/2D Arrays/Matrix.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+using namespace std;
+
+//       To print the elements in martrix form     
+int main() {
+    int students[3][3] = {{100,100,100},      // 0,1,2
+                        {76,54,62},           
+                         {32,83,71}};
+                
+      cout<<students[2][2];     // to print the element at 2nd row 2nd column
+}
