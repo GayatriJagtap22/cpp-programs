3cea4034b088b100fe3f4bb87be17bb41dd34399
Gayatri Jagtap <gayatrisjagtap.2806@gmail.com>
2026-03-16 19:11:48 +0530

    Update DiagonalSum.cpp

--- Programs/Arrays/2D Arrays/DiagonalSum.cpp
+++ Programs/Arrays/2D Arrays/DiagonalSum.cpp
@@ -27,3 +27,43 @@ int main() {
 
     return 0;
 }
+
+
+//  With O(n) time complexity
+int diagonalSum(int mat[][4], int n) {
+    int sum = 0;
+
+    for (int i = 0; i < n; i++) {          // rows
+        for (int j = 0; j < n; j++) {      // columns
+
+            if (i == j) {                  // primary diagonal
+                sum += mat[i][j];
+            }
+            else if (i + j == n - 1) {     // secondary diagonal
+                sum += mat[i][j];
+            }
+
+        }
+    }
+
+    return sum;
+}
+
+int main() {
+
+    int matrix2[3][3] = {     // odd matrix
+        {1,2,3},
+        {4,5,6},
+        {7,8,9}
+    };
+
+       int matrix[4][4] = {        // even matrix
+        {1, 2, 3, 4},
+        {5, 6, 7, 8},
+        {9, 10, 11, 12},
+        {13, 14, 15, 16} };
+
+    cout << diagonalSum(matrix, 4);
+
+    return 0;
+}
